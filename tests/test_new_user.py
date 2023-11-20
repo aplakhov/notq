24caf0d6064701cf703aa5783be9c8c200568ce6
finder <aplakhovprivate@gmail.com>
2023-11-20 21:09:16 +0300

    more tests

--- tests/test_new_user.py
+++ tests/test_new_user.py
@@ -12,31 +12,32 @@ def register_and_login(client, username, password):
 
 def assert_default_self_page(client, username):
     response = client.get('/u/' + username)
+    assert response.status_code == 200
     assert(username.encode() in response.data)
     assert('Этот пользователь пока ничего о себе не написал'.encode() in response.data)
 
-def assert_nondefault_self_page(client, username, text):
-    response = client.get('/u/' + username)
-    assert(username.encode() in response.data)
-    assert(text.encode() in response.data)
+def check_page_contains(client, url, what):
+    response = client.get(url)
+    assert response.status_code == 200
+    assert(what.encode() in response.data)
 
 def test_new_user(client):
-    register_and_login(client, "abc", "a")
-    assert_default_self_page(client, "abc")
+    register_and_login(client, 'abc', 'a')
+    assert_default_self_page(client, 'abc')
 
 def test_two_new_users(client):
-    register_and_login(client, "abc", "a")
+    register_and_login(client, 'abc', 'a')
     client.get('/auth/logout')
     
-    register_and_login(client, "def", "a")
+    register_and_login(client, 'def', 'a')
     
-    assert_default_self_page(client, "abc")
-    assert_default_self_page(client, "def")
+    assert_default_self_page(client, 'abc')
+    assert_default_self_page(client, 'def')
     
     assert client.get('/about').status_code == 200
     client.post('/about', data={'body': 'hello world'})
-    assert_nondefault_self_page(client, "def", "hello world")
-    assert_default_self_page(client, "abc")
+    check_page_contains(client, '/u/def', 'hello world')
+    assert_default_self_page(client, 'abc')
 
 def check_all_pages(client):
     assert client.get('/').status_code == 200
@@ -48,5 +49,11 @@ def check_all_pages(client):
 
 def test_empty_service(client):
     check_all_pages(client)
-    register_and_login(client, "abc", "a")
+    register_and_login(client, 'abc', 'a')
     check_all_pages(client)
+
+def test_logout(client):
+    register_and_login(client, 'abc', 'a')
+    client.get('/auth/logout')
+    check_page_contains(client, '/', 'Войти')
+    assert_default_self_page(client, 'abc')
