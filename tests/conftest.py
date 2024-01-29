076bcfd7e27a7748c64432d90a085c4c062a1b66
finder <aplakhovprivate@gmail.com>
2024-01-29 22:10:20 +0300

    remove unused code

--- tests/conftest.py
+++ tests/conftest.py
@@ -31,27 +31,3 @@ def app():
 @pytest.fixture
 def client(app):
     return app.test_client()
-
-
-@pytest.fixture
-def runner(app):
-    return app.test_cli_runner()
-
-
-class AuthActions(object):
-    def __init__(self, client):
-        self._client = client
-
-    def login(self, username='test', password='test'):
-        return self._client.post(
-            '/auth/login',
-            data={'username': username, 'password': password}
-        )
-
-    def logout(self):
-        return self._client.get('/auth/logout')
-
-
-@pytest.fixture
-def auth(client):
-    return AuthActions(client)
